8e40c92f897380b784db3340f2957ac317261417
SAKHRAOUI Omar <omediadon@gmail.com>
2024-03-03 22:25:59 +0100

    added utility methods for inputs

--- src/Elements/AbstractInput.php
+++ src/Elements/AbstractInput.php
@@ -11,11 +11,59 @@ abstract class AbstractInput extends HtmlElement{
 	];
 
 	public function __construct(string $type, string $name, array $attributes = [],){
-		$inputAttributes = [
+		$inputAttributes      = [
 			"type" => $type,
 			"name" => $name,
 		];
 		$this->defaultClasses = 'form-control';
 		parent::__construct("input", array_merge($inputAttributes, $attributes),);
+	}
+
+	public function name(string $name): static{
+		$this->attributes['name'] = $name;
+
+		return $this;
+	}
+
+	public function required(): static{
+		$this->attributes["required"] = '';
+
+		return $this;
+	}
+
+	public function placeholder(string $placeholder): static{
+		$this->attributes['placeholder'] = $placeholder;
+
+		return $this;
+	}
+
+	public function autoFocus(): static{
+		$this->attributes['autofocus'] = '';
+
+		return $this;
+	}
+
+	public function readOnly(): static{
+		$this->attributes['readonly'] = '';
+
+		return $this;
+	}
+
+	public function maxLength(int $length): static{
+		$this->attributes['maxlength'] = $length;
+
+		return $this;
+	}
+
+	public function minLength(int $length): static{
+		$this->attributes['minlength'] = $length;
+
+		return $this;
+	}
+
+	public function value(string $value): static{
+		$this->attributes['value'] = $value;
+
+		return $this;
 	}
 }
