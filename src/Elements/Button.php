3e389935c1f2271a82e28f2b13df6303abb1b16c
SAKHRAOUI Omar <omediadon@gmail.com>
2024-03-05 01:44:21 +0100

    added type as default attribute of buttons

--- src/Elements/Button.php
+++ src/Elements/Button.php
@@ -5,9 +5,17 @@ namespace Xana\GenHtml\Elements;
 use Xana\GenHtml\HtmlElement;
 
 class Button extends HtmlElement{
-	public function __construct(string $text, array $attributes = []){
-		parent::__construct("button", $attributes);
-		$this->defaultClasses    = 'btn btn-primary';
+	protected array $dontFlush = [
+		"type"
+	];
+
+	public function __construct(string $text, string $type = "button", array $attributes = []){
+		$inputAttributes      = [
+			"type" => $type,
+		];
+		$this->defaultClasses = 'btn btn-primary';
+		parent::__construct("button", array_merge($inputAttributes, $attributes));
+		$this->defaultClasses = 'btn btn-primary';
 		$this->setText($text);
 	}
 }
